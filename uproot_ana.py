376569727e1e33818e6c5cb2aed81b7daae576ec
Anna Giannakou <agiannakou@lbl.gov>
2024-05-13 13:42:22 -0700

    psutil for streaming

--- uproot_ana.py
+++ uproot_ana.py
@@ -1,25 +1,49 @@
-import uproot
+#import uproot
 import awkward as ak
 import logging
+import psutil
+import time
 
 
+def monitor(func):
+    def wrapper(*args, **kwargs):
+        process = psutil.Process()
+        cur_func_pid = process.pid
+        process = psutil.Process(cur_func_pid)
 
-stride = 1
-interesting_vars = ["Photon_pt", "Photon_eta", "Photon_phi", "MET_pt", "Jet_pt"]
-##adding logger for timestamps
-logging.basicConfig(filename='timestamp_logs',
-                    filemode='a',
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S.%f'
-)
+        func(*args, **kwargs)
 
-if __name__ == "__main__":
+        cpu_usage = process.cpu_percent(interval=0.01)
+        memory_usage = process.memory_info().rss / 1024 / 1024 / 1024 # bytes -> GB
+        net_counters = process.connections()
+        print(f"io_counters.bytes_sent:{net_counters}%")
+        print(f"CPU usage: {cpu_usage}%")
+        print(f"Memory usage: {memory_usage}GB")
+        mem = process.memory_percent()
+        for child in process.children(recursive=True):
+            cpu_usage_child = child.cpu_percent()
+            print(f"Child CPU usage: {cpu_usage_child}%")
+
+    return wrapper
+
+@monitor
+def workflow():
+    stride = 1
+    interesting_vars = ["Photon_pt", "Photon_eta", "Photon_phi", "MET_pt", "Jet_pt"]
+    ##adding logger for timestamps
+    logging.basicConfig(filename='timestamp_logs',filemode='a',
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S.%f'
+    )
+    print('la')
     with open("pfns1.txt") as fin:
         file_pfns = list(l.strip() for l in fin)
-
-    for pfn in file_pfns[::stride]:
-        logging.info('Opening file {}'.format(pfn))
+        for pfn in file_pfns[::stride]:
+            print('in')
+            logging.info('Opening file {}'.format(pfn))
+            #time.sleep(1)
+        #exit(0)
         with uproot.open(pfn) as fin:
             some_data = fin["Events"].arrays(interesting_vars)
             for var in interesting_vars:
@@ -30,3 +54,6 @@ if __name__ == "__main__":
             # Check the actual data server
             dataserver: str = fin.file.source.file.get_property("DataServer")
             logging.info('File was served from {}'.format(dataserver))
+
+
+workflow()
